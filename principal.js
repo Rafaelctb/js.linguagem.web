717d07edc7cebdcbce99a3590c63515a244eb209
Rafaelctb <127303930+Rafaelctb@users.noreply.github.com>
2023-08-30 08:30:27 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,3 +1,18 @@
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura")
+var altura = tdAltura.textContent;
+
+console.log(paciente); //tr
+console.log(tdPeso); //td que tem peso
+console.log(peso); //obter100
+
+var imc = peso / altura * altura; // 100 / 2.0 * 2.0 = 100 / 4 =>>>> 25 
+
+console.log(imc);
