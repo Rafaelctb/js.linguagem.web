2da0e13fa8a63c6bad89edb53586f5405e5209aa
Rafaelctb <127303930+Rafaelctb@users.noreply.github.com>
2023-09-01 09:03:11 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,14 +1,31 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var paciente = document.querySelectorAll("paciente");
+
+console.log(pacientes);
 
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura")
+var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValido = true;
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if(alturaEhValido && pesoEhValido){
+    var imc = peso / (altura + altura);
+    tdImc.textContent = imc;
+}
 console.log(paciente); //tr
 console.log(tdPeso); //td que tem peso
 console.log(peso); //obter100
